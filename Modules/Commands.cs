8ce4763816562c2d369abdb2eddd1df1cf35987f
SindreMA <SindreMA@gmail.com>
2023-09-18 23:32:16 +0200

    first commit

---
+++ Modules/Commands.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("Ping")]
+        public async Task Ping()
+        {
+            await Context.Channel.SendMessageAsync("Pong!");
+        }
+        [Command("sendmsg")]
+        public async Task sendmsg(ulong channelid, [Remainder]string text)
+        {
+            try
+            {
+                var chan = Context.Client.GetChannel(channelid);
+                var channel = (chan as SocketTextChannel);
+                await channel.SendMessageAsync(text);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# SendMsgBot
+
+## Description
+A simple bot to send messages from a bot.
